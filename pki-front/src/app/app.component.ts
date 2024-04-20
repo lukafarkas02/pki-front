446ea6faa562152e93862e3c333c313a5b1b8989
bojangelic <bojangelic417@gmail.com>
2024-04-20 18:58:45 +0200

    Form for creating new certificate

--- pki-front/src/app/app.component.ts
+++ pki-front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CreateCertificateComponent} from "./create-certificate/create-certificate.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CreateCertificateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- pki-front/src/app/app.routes.ts
+++ pki-front/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {CreateCertificateComponent} from "./create-certificate/create-certificate.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'create-certificate', component: CreateCertificateComponent}
+];
