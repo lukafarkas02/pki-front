18778658b409d8643eeaae871b96e5c9744bbb24
bojangelic <bojangelic417@gmail.com>
2024-04-20 22:10:58 +0200

    Bug fixed

--- pki-front/src/app/app.routes.ts
+++ pki-front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 
 import {CeratificatesListComponent} from "./certificates/ceratificates-list/ceratificates-list.component";
+import {LoginPageComponent} from "./login/login-page/login-page.component";
+import {CreateCertificateComponent} from "./create-certificate/create-certificate.component";
 
 export const routes: Routes = [
   {path: 'certificates-list', component: CeratificatesListComponent},
